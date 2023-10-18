71e8dd81488e40c2f483382bbcb14803e654c226
Salman ElHussini <hussainyred@gmail.com>
2023-10-18 07:57:30 +0300

    hw complete

--- tempclone/main.h
+++ tempclone/main.h
@@ -7,11 +7,10 @@
 /*push adds a new data item to stack*/
 int push(char newest_item, char *stack, int *index, int size);
 /*pop takes out the current item on top of stack*/
-int pop();
+int pop(char *stack, int *index);
 /*checks if stack is empty (non existent)*/
-int isempty();
+int isempty(char *stack);
 /*checks what data item is currently at the top of stack*/
-int top();
-
+int top(char *stack, int *index);
 
 #endif
--- tempclone/stackprac.c
+++ tempclone/stackprac.c
@@ -1,6 +1,8 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int push(char newest_item, char *stack, int *index, int size) {
+int push(char newest_item, char *stack, int *index, int size) 
+{
     if (*index >= size) {
 	        printf("insufficient memory available\n");
 	        return (-1);
@@ -13,6 +15,31 @@ int push(char newest_item, char *stack, int *index, int size) {
     return (*index);
 }
 
+int isempty(char *stack)
+{
+	if (!stack)
+		printf("stack is empty\n");
+	else
+		printf("stack isn't empty\n");
+
+	return (0);
+}
+
+int pop(char *stack, int *index)
+{
+	(*index)--;
+
+	stack[*index] = '\0';
+	(*index)--;
+
+	return (0);
+}
+
+int top(char *stack,int *index)
+{
+	printf("%c with index %i is currently at the top of stack\n", stack[*index],*index);
+	return (0);
+}
 int main(int argc, char **argv) {
     int index = 0, size = 10;
     char stack[10] = {0};
@@ -21,6 +48,10 @@ int main(int argc, char **argv) {
     push(x, stack, &index, 10);
     push(y, stack, &index, 10);
     push(z, stack, &index, 10);
+
+	pop(stack, &index);
+	top(stack, &index);
+	isempty(stack);
 
     return 0;
 }
